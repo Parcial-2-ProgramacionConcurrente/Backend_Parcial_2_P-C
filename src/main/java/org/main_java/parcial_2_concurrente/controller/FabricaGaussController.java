dc9413da5cd2be59136bf5c31d235d027dcd0991
Jaime Lopez Diaz <jlopedia@myuax.com>
2024-11-13 20:19:05 +0100

    -

--- src/main/java/org/main_java/parcial_2_concurrente/controller/FabricaGaussController.java
+++ src/main/java/org/main_java/parcial_2_concurrente/controller/FabricaGaussController.java
@@ -2,7 +2,6 @@ package org.main_java.parcial_2_concurrente.controller;
 
 import org.main_java.parcial_2_concurrente.model.fabricaDTO.FabricaGaussDTO;
 import org.main_java.parcial_2_concurrente.service.fabricaService.FabricaGaussService;
-import org.main_java.parcial_2_concurrente.service.galtonService.GaltonBoardService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -16,14 +15,6 @@ public class FabricaGaussController {
     @Autowired
     private FabricaGaussService fabricaGaussService;
 
-    @Autowired
-    private GaltonBoardService galtonBoardService;
-
-    @PostMapping
-    public Mono<FabricaGaussDTO> createFabrica(@RequestBody FabricaGaussDTO fabricaGaussDTO) {
-        return fabricaGaussService.createFabrica(fabricaGaussDTO);
-    }
-
     @GetMapping
     public Flux<FabricaGaussDTO> findAllFabricas() {
         return fabricaGaussService.findAllFabricas();
@@ -33,20 +24,6 @@ public class FabricaGaussController {
     public Mono<ResponseEntity<FabricaGaussDTO>> findFabricaById(@PathVariable String id) {
         return fabricaGaussService.findFabricaById(id)
                 .map(ResponseEntity::ok)
-                .defaultIfEmpty(ResponseEntity.notFound().build());
-    }
-
-    @PutMapping("/{id}")
-    public Mono<ResponseEntity<FabricaGaussDTO>> updateFabrica(@PathVariable String id, @RequestBody FabricaGaussDTO fabricaGaussDTO) {
-        return fabricaGaussService.updateFabrica(id, fabricaGaussDTO)
-                .map(ResponseEntity::ok)
-                .defaultIfEmpty(ResponseEntity.notFound().build());
-    }
-
-    @DeleteMapping("/{id}")
-    public Mono<ResponseEntity<Void>> deleteFabricaById(@PathVariable String id) {
-        return fabricaGaussService.deleteFabricaById(id)
-                .then(Mono.just(ResponseEntity.noContent().<Void>build()))
                 .defaultIfEmpty(ResponseEntity.notFound().build());
     }
 
